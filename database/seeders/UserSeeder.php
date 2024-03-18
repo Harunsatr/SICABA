f7bc2dca1ef6f2061611c01653da34a1b8c5a646
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2024-03-18 12:26:47 +0700

    ubah nama admin

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -14,7 +13,7 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         $admin = User::create([
-            'name' => 'admin',
+            'name' => 'Harun Satrio',
             'email' => 'admin@mail.com',
             'password' => bcrypt('admin12345'),
         ]);
