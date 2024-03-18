20c01aab9cf3eaf3cac4d20125d7818d9d4dadc5
Harunsatr <harunsatr36@gmail.com>
2024-03-18 11:58:01 +0700

    pembaruan logo

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -22,32 +22,7 @@
         }
     </style>
 
-</head><!--
-    =========================================================
-    * Soft UI Dashboard - v1.0.3
-    =========================================================
-    
-    * Product Page: https://www.creative-tim.com/product/soft-ui-dashboard
-    * Copyright 2021 Creative Tim (https://www.creative-tim.com)
-    * Licensed under MIT (https://www.creative-tim.com/license)
-    
-    * Coded by Creative Tim
-    
-    =========================================================
-    
-    * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-    -->
-    <!DOCTYPE html>
-    <html lang="en">
-    
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-        <link rel="icon" type="image/png" href="../assets/img/favicon.png">
-        <title>
-            Soft UI Dashboard by Creative Tim
-        </title>
+</head>
         <!--     Fonts and icons     -->
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
         <!-- Nucleo Icons -->
@@ -59,7 +34,7 @@
         <!-- CSS Files -->
         <link id="pagestyle" href={{asset("softui/css/soft-ui-dashboard.css?v=1.0.3")}} rel="stylesheet" />
     </head>
-    
+
     <body class="g-sidenav-show  bg-gray-100">
         <section class="min-vh-100 mb-8">
             <div class="page-header align-items-start min-vh-50 pt-5 pb-11 m-3 border-radius-lg"
@@ -102,23 +77,6 @@
                                         </svg>
                                     </a>
                                 </div>
-                                <div class="col-3 px-1">
-                                    <a class="btn btn-outline-light w-100" href="javascript:;">
-                                        <svg width="24px" height="32px" viewBox="0 0 64 64" version="1.1"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            xmlns:xlink="http://www.w3.org/1999/xlink">
-                                            <g id="Artboard" stroke="none" stroke-width="1" fill="none"
-                                                fill-rule="evenodd">
-                                                <g id="apple-black" transform="translate(7.000000, 0.564551)"
-                                                    fill="#000000" fill-rule="nonzero">
-                                                    <path
-                                                        d="M40.9233048,32.8428307 C41.0078713,42.0741676 48.9124247,45.146088 49,45.1851909 C48.9331634,45.4017274 47.7369821,49.5628653 44.835501,53.8610269 C42.3271952,57.5771105 39.7241148,61.2793611 35.6233362,61.356042 C31.5939073,61.431307 30.2982233,58.9340578 25.6914424,58.9340578 C21.0860585,58.9340578 19.6464932,61.27947 15.8321878,61.4314159 C11.8738936,61.5833617 8.85958554,57.4131833 6.33064852,53.7107148 C1.16284874,46.1373849 -2.78641926,32.3103122 2.51645059,22.9768066 C5.15080028,18.3417501 9.85858819,15.4066355 14.9684701,15.3313705 C18.8554146,15.2562145 22.5241194,17.9820905 24.9003639,17.9820905 C27.275104,17.9820905 31.733383,14.7039812 36.4203248,15.1854154 C38.3824403,15.2681959 43.8902255,15.9888223 47.4267616,21.2362369 C47.1417927,21.4153043 40.8549638,25.1251794 40.9233048,32.8428307 M33.3504628,10.1750144 C35.4519466,7.59650964 36.8663676,4.00699306 36.4804992,0.435448578 C33.4513624,0.558856931 29.7884601,2.48154382 27.6157341,5.05863265 C25.6685547,7.34076135 23.9632549,10.9934525 24.4233742,14.4943068 C27.7996959,14.7590956 31.2488715,12.7551531 33.3504628,10.1750144"
-                                                        id="Shape"></path>
-                                                </g>
-                                            </g>
-                                        </svg>
-                                    </a>
-                                </div>
                                 <div class="col-3 me-auto px-1">
                                     <a class="btn btn-outline-light w-100" href="javascript:;">
                                         <svg width="24px" height="32px" viewBox="0 0 64 64" version="1.1"
@@ -155,7 +113,7 @@
                             <div class="card-body">
                                 <form method="POST" action="{{ route('register') }}">
                                     @csrf
-                                
+
                                     <!-- Name -->
                                     <div class="mb-3">
                                         <input type="text" class="form-control" placeholder="Name" aria-label="Name" aria-describedby="name-addon" name="name" id="name" value="{{ old('name') }}" required autofocus autocomplete="name">
@@ -163,15 +121,15 @@
                                             <p class="text-sm text-red-500 mt-1">{{ $message }}</p>
                                         @enderror
                                     </div>
-                                
+
                                     <!-- Email Address -->
                                     <div class="mb-3">
                                         <input type="email" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="email-addon" name="email" id="email" value="{{ old('email') }}" required autocomplete="email">
                                         @error('email')
                                             <p class="text-sm text-red-500 mt-1">{{ $message }}</p>
                                         @enderror
-                                    </div>  
-                                
+                                    </div>
+
                                     <!-- Password -->
                                     <div class="mb-3">
                                         <input type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="password-addon" name="password" id="password" required autocomplete="new-password">
@@ -179,78 +137,24 @@
                                             <p class="text-sm text-red-500 mt-1">{{ $message }}</p>
                                         @enderror
                                     </div>
-                                
+
                                     <!-- Confirm Password -->
                                     <div class="mb-3">
                                         <input type="password" class="form-control" placeholder="Confirm Password" aria-label="Confirm Password" aria-describedby="confirm-password-addon" name="password_confirmation" id="password_confirmation" required autocomplete="new-password">
                                     </div>
-                                
+
                                     <div class="flex items-center justify-end mt-4">
                                         <a href="{{ route('login') }}" class="text-sm text-gray-600 underline">Already registered?</a>
-                                
+
                                         <button type="submit" class="register-btn btn w-100 my-4 mb-2">Register</button>
                                     </div>
-                                </form>                                                              
+                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
-        <!-- -------- START FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
-        <footer class="footer py-5">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-8 mb-4 mx-auto text-center">
-                        <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-                            Company
-                        </a>
-                        <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-                            About Us
-                        </a>
-                        <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-                            Team
-                        </a>
-                        <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-                            Products
-                        </a>
-                        <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-                            Blog
-                        </a>
-                        <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-                            Pricing
-                        </a>
-                    </div>
-                    <div class="col-lg-8 mx-auto text-center mb-4 mt-2">
-                        <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
-                            <span class="text-lg fab fa-dribbble"></span>
-                        </a>
-                        <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
-                            <span class="text-lg fab fa-twitter"></span>
-                        </a>
-                        <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
-                            <span class="text-lg fab fa-instagram"></span>
-                        </a>
-                        <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
-                            <span class="text-lg fab fa-pinterest"></span>
-                        </a>
-                        <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
-                            <span class="text-lg fab fa-github"></span>
-                        </a>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-8 mx-auto text-center mt-1">
-                        <p class="mb-0 text-secondary">
-                            Copyright ©
-                            <script>
-                                document.write(new Date().getFullYear())
-                            </script> Soft by Creative Tim.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </footer>
         <!-- -------- END FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
         <!--   Core JS Files   -->
         <script src={{asset("softui/js/core/popper.min.js")}}></script>
@@ -271,7 +175,7 @@
         <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
         <script src={{asset("softui/js/soft-ui-dashboard.min.js?v=1.0.3")}}></script>
     </body>
-    
+
     </html>
 
     @endsection
